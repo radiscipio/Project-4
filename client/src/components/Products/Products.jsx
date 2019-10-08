fbfdbd29e52dd14fce0dbd981d2abc94b5c13774
Radiscipio <robertdiscipio@gmail.com>
2019-10-08 10:08:07 -0400

    Seed data

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import './products.css'
 
 function Products() {
   return (
     <div>
-      This is Products
+      <div className='products-container'>
+        <div>This is Products</div>
+      </div>
     </div>
   )
 }
